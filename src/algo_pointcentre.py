6f3c4ce1361257ac7d73ade980a6c331ea7f5949
Lucas Ivan <ivan.lucasreiner@gmail.com>
2023-03-30 18:43:46 +0200

    Correction algo point centré (focntionnel)  -> enregistre l'image dans resources/result.jpg au lieu de l'afficher

--- src/algo_pointcentre.py
+++ src/algo_pointcentre.py
@@ -6,40 +6,43 @@ def get_sztr():
 
 
 def paint(trame, x, y, base, treshold, cpt):
-    # print(x, y)
     if (cpt < treshold):
-        trame[x][y] = base
-    else:
         trame[x][y] = 255 - base
-    cpt += 1
 
 
-def getTrame(x, y, intensity, sizeZone):
-    trame = [[0] * sizeZone] * sizeZone
+def getTrame(intensity, sizeZone):
     base = 0
     if (intensity > 127):
         base = 255
+    trame = [[base for i in range(sizeZone+1)] for j in range(sizeZone+1)]
     treshold = (intensity/255)*sizeZone*sizeZone
-    print("treshold", treshold, "sizeZone//2", sizeZone//2)
-    cx, cy = x + sizeZone//2, y + sizeZone//2
-    for m in range(sizeZone//2):
-        cpt = 0
-        print("1m", m)
-        for i in range(-m, m+1):
-            print(i)
-            paint(trame, cx+i, cy+m, base, treshold, cpt)
-        print("2m", m)
-        for i in range(m, -m-1, -1):
-            print(i)
+    cx, cy = sizeZone//2, sizeZone//2
+    cpt = 1
+    m = 1
+    paint(trame, cx, cy, base, treshold, cpt)
+    cpt += 1
+    while (cpt < treshold and cpt < sizeZone*sizeZone):
+        for i in range(1-m, m):
+            paint(trame, cx+i, cy-m, base, treshold, cpt)
+            cpt += 1
+        for i in range(1-m, m):
             paint(trame, cx+m, cy+i, base, treshold, cpt)
-        print("3m", m)
-        for i in range(-m, m+1):
-            print(i)
-            paint(trame, cx+i, cy+m, base, treshold, cpt)
-        print("4m", m)
-        for i in range(m, -m-1, -1):
-            print(i)
-            paint(trame, cx+m, cy+i, base, treshold, cpt)
+            cpt += 1
+        for i in range(1-m, m):
+            paint(trame, cx-i, cy+m, base, treshold, cpt)
+            cpt += 1
+        for i in range(1-m, m):
+            paint(trame, cx-m, cy-i, base, treshold, cpt)
+            cpt += 1
+        paint(trame, cx-m, cy-m, base, treshold, cpt)
+        cpt += 1
+        paint(trame, cx+m, cy-m, base, treshold, cpt)
+        cpt += 1
+        paint(trame, cx+m, cy+m, base, treshold, cpt)
+        cpt += 1
+        paint(trame, cx-m, cy+m, base, treshold, cpt)
+        cpt += 1
+        m += 1
     return trame
 
 
@@ -48,16 +51,23 @@ def pointCentre(img, sizeTrame):
 
     width, height = img.size
     print(width, height)
+    cpt = 0
 
     for x in range(0, width-sizeTrame, sizeTrame):
         for y in range(0, height-sizeTrame, sizeTrame):
+            cpt += 1
             zoneIntensity = 0
+            change = 0
             for i in range(sizeTrame):
                 for j in range(sizeTrame):
-                    zoneIntensity += pixels[x+i, y+j]/sizeTrame
-            print("intensity", x, y, zoneIntensity)
-            trame = getTrame(x, y, zoneIntensity, sizeTrame)
+                    if (pixels[x+i, y+j] != 0 and pixels[x+i, y+j] != 255):
+                        change = 1
+                    zoneIntensity += pixels[x+i, y+j]/(sizeTrame*sizeTrame)
+            if (change == 0):
+                continue
+            trame = getTrame(zoneIntensity, sizeTrame)
             for i in range(sizeTrame):
                 for j in range(sizeTrame):
+                    tmp = pixels[x+i, y+j]
                     pixels[x+i, y+j] = trame[i][j]
-    img.show()
+    img.save("resources/result.jpg", "jpeg")
--- src/projectlib.py
+++ src/projectlib.py
@@ -5,10 +5,13 @@ def switch_algo(algo, img):
 	if algo == 1 :
 		seuil = thresholding.get_ths()
 		thresholding.threshold(img,seuil)
+		return
 	if algo == 2 :
 		sizeTrame = algo_pointcentre.get_sztr()
 		algo_pointcentre.pointCentre(img,sizeTrame)
+		return
 	else:
-		print('rien\n')
+		print('Algorithme inconnu\n')
+		return
 
         
